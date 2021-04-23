0e5af3b39ab4a15dc2764ccc4ff7f473d918979b
Softonic254 <rotichenoch27@gmail.com>
2021-04-23 11:06:26 +0300

    It will add styling to table and buttons

--- js/script.js
+++ js/script.js
@@ -7,21 +7,25 @@ $(document).ready(function () {
     });
 });
 
-function Total(size, crust, topping) {
+function Total(size, crust, topping, delivery, quantity) {
     this.size = size;
     this.crust = crust;
     this.topping = topping;
+    this.delivery = delivery;
+    this.quantity = quantity;
 }
-
 
 function pick() {
     let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
     let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
     let selectedSize = parseFloat(document.getElementById("pizza-size").value);
+    let quantity = parseFloat(document.getElementById("pizzaNumber").value);
+    let delivery = document.getElementById("deliver").value;
 
-    const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, 0);
+    const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, delivery);
     // const deliver = new Total(selectedSize + selectedCrust + selectedToppings + selectedDelivery);
 
-    var cost = pickDelivery.size + pickDelivery.crust + pickDelivery.topping + pickDelivery.delivery
-    alert(cost);
+    var cost = (pickDelivery.size + pickDelivery.crust + pickDelivery.topping + pickDelivery.delivery) * pickDelivery.quantity;
+    var totalCost = (quantity * cost);
+    alert(typeof(cost1));
 }
