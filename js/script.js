4c30425a7d876aab81d3a44733bdf2b520243726
Softonic254 <rotichenoch27@gmail.com>
2021-04-23 11:10:01 +0300

    It will collect and validate input data and call appropriate function

--- js/script.js
+++ js/script.js
@@ -1,31 +1,78 @@
 $(document).ready(function () {
-    $("h4.bg-order").addClass("order");
+    $("#bg-order").addClass("order");
+    $("#summary").hide();
     $(".order-form").hide();
-    $("h4.bg-order").on('click', function () {
-        $(".landing").hide();
+    $("#bg-order").on('click', function () {
+        $("#landing").hide();
         $(".order-form").show();
+    });
+
+    $("#pick").on('click', function () {
+        $(".order-form").hide();
+        validatePick();
+    });
+
+    $("#deliver").on('click', function () {
+        validateDeliver();
     });
 });
 
-function Total(size, crust, topping, delivery, quantity) {
+function Total(size, crust, topping, delivery) {
     this.size = size;
     this.crust = crust;
     this.topping = topping;
     this.delivery = delivery;
-    this.quantity = quantity;
 }
 
-function pick() {
+
+function validatePick() {
     let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
     let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
     let selectedSize = parseFloat(document.getElementById("pizza-size").value);
-    let quantity = parseFloat(document.getElementById("pizzaNumber").value);
-    let delivery = document.getElementById("deliver").value;
+    let quantity = document.getElementById("pizzaNumber").value;
+    let selectedDelivery = parseFloat(document.getElementById("pick").value);
 
-    const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, delivery);
-    // const deliver = new Total(selectedSize + selectedCrust + selectedToppings + selectedDelivery);
+    const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
+    // const deliver = new Total(selectedSize + selectedCrust + selectedToppings + delivery);
 
-    var cost = (pickDelivery.size + pickDelivery.crust + pickDelivery.topping + pickDelivery.delivery) * pickDelivery.quantity;
-    var totalCost = (quantity * cost);
-    alert(typeof(cost1));
+    var cost = pickDelivery.size + pickDelivery.crust + pickDelivery.topping + pickDelivery.delivery
+    let totalCost = cost * quantity;
+
+    if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
+        alert("Select all items.");
+    } else {
+        $("#order-form").hide();
+    }
+    $("#itemQuantity").text(quantity);
+    $("#itemSize").text(selectedSize);
+    $("#itemCrust").text(selectedCrust);
+    $("#itemTopping").text(selectedToppings);
+    $("#totalValue").text("Kshs. " + totalCost);
+
+
+}
+
+function validateDeliver() {
+    let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
+    let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
+    let selectedSize = parseFloat(document.getElementById("pizza-size").value);
+    let selectedDelivery = parseFloat(document.getElementById("deliver").value);
+    let quantity = document.getElementById("pizzaNumber").value;
+
+    const deliver = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
+    // const deliver = new Total(selectedSize + selectedCrust + selectedToppings + delivery);
+
+    var cost = deliver.size + deliver.crust + deliver.topping + deliver.delivery
+    let totalCost = cost * quantity;
+    if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
+        alert("Select all items.");
+    } else {
+        return totalCost;
+    }
+    $("#itemQuantity").text(quantity);
+    $("#itemSize").text(selectedSize);
+    $("#itemCrust").text(selectedCrust);
+    $("#itemTopping").text(selectedToppings);
+    $("#totalValue").text("Kshs. " + totalCost);
+
 }
