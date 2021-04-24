4ef0a4585b9ea4114802ec03fd74e32f3bf69d07
Softonic254 <rotichenoch27@gmail.com>
2021-04-24 19:46:03 +0300

    It will display receipt summary

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 $(document).ready(function () {
     $("#bg-order").addClass("order");
-    $("#summary").hide();
+    $(".summary").hide();
+    $(".receipt").hide();
     $(".order-form").hide();
+
     $("#bg-order").on('click', function () {
         $("#landing").hide();
         $(".order-form").show();
     });
 
     $("#pick").on('click', function () {
-        $(".order-form").hide();
-        validatePick();
+
+        if (validatePick() != NaN) {
+            validatePick();
+        } else {
+            $(".order-form").show();
+            $(".summary").hide();
+        }
     });
 
     $("#deliver").on('click', function () {
-        validateDeliver();
+
+        if (validateDeliver() != NaN) {
+            validateDeliver();
+        } else {
+            $(".order-form").show();
+            $(".summary").hide();
+        }
     });
+
+    $(".checkOut").on('click', function () {
+        $(".order-form").show();
+        $(".receipt").show();
+        $(".summary").hide(); 7
+        checkOut();
+    });
+
 });
 
 function Total(size, crust, topping, delivery) {
@@ -39,16 +60,16 @@ function validatePick() {
     let totalCost = cost * quantity;
 
     if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
-        alert("Select all items.");
+        alert(noInput);
     } else {
-        $("#order-form").hide();
-    }
-    $("#itemQuantity").text(quantity);
-    $("#itemSize").text(selectedSize);
-    $("#itemCrust").text(selectedCrust);
-    $("#itemTopping").text(selectedToppings);
-    $("#totalValue").text("Kshs. " + totalCost);
+        $(".order-form").hide();
+        $(".summary").show();
+        document.getElementById("itemQuantity").innerHTML = quantity;
+        document.getElementById("itemSize").innerHTML = selectedSize;
+        document.getElementById("itemCrust").innerHTML = selectedCrust;
+        document.getElementById("totalValue").innerHTML = totalCost;
 
+    }
 
 }
 
@@ -65,14 +86,40 @@ function validateDeliver() {
     var cost = deliver.size + deliver.crust + deliver.topping + deliver.delivery
     let totalCost = cost * quantity;
     if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
-        alert("Select all items.");
+        alert(noInput);
     } else {
-        return totalCost;
+        confirm(charge);
+        if (charge == true) {
+            $(".order-form").hide();
+            $(".summary").show();
+        } else {
+            return false;
+        }
     }
-    $("#itemQuantity").text(quantity);
-    $("#itemSize").text(selectedSize);
-    $("#itemCrust").text(selectedCrust);
-    $("#itemTopping").text(selectedToppings);
-    $("#totalValue").text("Kshs. " + totalCost);
+    if (charge) {
+        $("#itemQuantity").text(quantity);
+        $("#itemSize").text(selectedSize);
+        $("#itemCrust").text(selectedCrust);
+        $("#itemTopping").text(selectedToppings);
+        $("#totalValue").text(totalCost);
+    }
+}
+
+function checkOut() {
+    let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
+    let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
+    let selectedSize = parseFloat(document.getElementById("pizza-size").value);
+    let selectedDelivery = parseFloat(document.getElementById("deliver").value);
+    let quantity = document.getElementById("pizzaNumber").value;
+    let total = (selectedCrust + selectedToppings + selectedSize + selectedDelivery) * quantity;
+    $("#quantity").text(quantity);
+    $("#size").text(selectedSize);
+    $("#crust").text(selectedCrust);
+    $("#topping").text(selectedToppings);
+    $("#value").text(total);
 
 }
+
+var charge = "There will be an additional charge for transport";
+var noInput = "Kindly provide all fields";
+var confirmTransport = "Are you sure you don't need delivery?";
