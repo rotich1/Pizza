915ddbda5bbc50666882d38dfdbb27a551382789
Softonic254 <rotichenoch27@gmail.com>
2021-04-25 12:47:35 +0300

    It will divide UI logic and business logic

--- js/script.js
+++ js/script.js
@@ -1,61 +1,4 @@
 //BUSINESS LOGIC
-$(document).ready(function () {
-    $("#bg-order").addClass("order");
-    $(".summary").hide();
-    $(".receipt").hide();
-    $(".order-form").hide();
-    $(".print").hide();
-    $(".alert").hide();
-
-    //The code below will hide the landing page and display the form to user
-    $("#bg-order").on('click', function () {
-        $("#landing").hide();
-        $(".order-form").show();
-    });
-
-    //The code below executes customers inputs for those who are coming the pick their pizza
-    $("#pick").on('click', function () {
-
-        if (validatePick() != NaN) {
-            validatePick();
-        } else {
-            $(".order-form").show();
-            $(".summary").hide();
-        }
-    });
-
-    //The code below executes customers inputs for those who need delivery their pizza
-
-    $("#deliver").on('click', function () {
-
-        if (validateDeliver() != NaN) {
-            validateDeliver();
-        } else {
-            $(".order-form").show();
-            $(".summary").hide();
-
-        }
-    });
-
-    //The code below will show a receipt of what customer has ordered
-
-    $(".checkOut").on('click', function () {
-        $(".order-form").show();
-        $(".receipt").show();
-        $(".summary").hide();
-        $(".print").show();
-        checkOut();
-    });
-
-    $(".print").on('click', function () {
-        window.print();
-    });
-
-    $("p.homeLink").on("click", function () {
-        location.reload();
-    });
-
-});
 
 //A function constructor
 function Total(size, crust, topping, delivery) {
@@ -87,6 +30,7 @@ function validatePick() {
         document.getElementById("itemQuantity").innerHTML = quantity;
         document.getElementById("itemSize").innerHTML = selectedSize;
         document.getElementById("itemCrust").innerHTML = selectedCrust;
+        document.getElementById("itemTopping").innerHTML = selectedToppings;
         document.getElementById("totalValue").innerHTML = totalCost;
 
     }
@@ -108,7 +52,7 @@ function validateDeliver() {
     if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
         $(".alert").show();
     } else {
-        confirm(charge);
+        $(".alertDeliver").show();
         if (charge) {
             $(".order-form").hide();
             $(".summary").show();
@@ -131,7 +75,7 @@ function checkOut() {
     let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
     let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
     let selectedSize = parseFloat(document.getElementById("pizza-size").value);
-    let selectedDelivery = parseFloat(document.getElementById("deliver").value);
+    let selectedDelivery = parseFloat(document.getElementById("pick").value);
     let quantity = document.getElementById("pizzaNumber").value;
     let total = (selectedCrust + selectedToppings + selectedSize + selectedDelivery) * quantity;
     $("#quantity").text(quantity);
@@ -139,7 +83,6 @@ function checkOut() {
     $("#crust").text(selectedCrust);
     $("#topping").text(selectedToppings);
     $("#value").text(total);
-
 }
 
 
@@ -148,3 +91,63 @@ function checkOut() {
 var charge = "There will be an additional charge for transport";
 var noInput = "Kindly provide all fields";
 var confirmTransport = "Are you sure you don't need delivery?";
+
+$(document).ready(function () {
+    $("#bg-order").addClass("order");
+    $(".summary").hide();
+    $(".receipt").hide();
+    $(".order-form").hide();
+    $(".print").hide();
+    $(".alert").hide();
+    $(".alertDeliver").hide();
+
+    //The code below will hide the landing page and display the form to user
+    $("#bg-order").on('click', function () {
+        $("#landing").hide();
+        $(".order-form").show();
+    });
+
+    //The code below executes customers inputs for those who are coming the pick their pizza
+    $("#pick").on('click', function () {
+
+        if (validatePick() != NaN) {
+            validatePick();
+        } else {
+            $(".order-form").show();
+            $(".summary").hide();
+        }
+    });
+
+    //The code below executes customers inputs for those who need delivery their pizza
+
+    $("#deliver").on('click', function () {
+
+        if (validateDeliver() != NaN) {
+            validateDeliver();
+        } else {
+            $(".order-form").show();
+            $(".summary").hide();
+        }
+    });
+
+    //The code below will show a receipt of what customer has ordered
+
+    $(".checkOut").on('click', function () {
+        $(".order-form").show();
+        $(".receipt").show();
+        $(".summary").hide();
+        $(".print").show();
+        $(".alert").hide();
+        $(".alertDeliver").hide();
+        checkOut();
+    });
+
+    $(".print").on('click', function () {
+        window.print();
+    });
+
+    $("p.homeLink").on("click", function () {
+        location.reload();
+    });
+
+});
